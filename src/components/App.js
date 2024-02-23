bf6ae6b4dea23ce28320ddc78038b38cf754c4ed
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-02-23 17:26:16 -0500

    taskArray populating with unique id

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
+import React, { useState } from 'react';
+import CategoryFilter from './CategoryFilter';
+import NewTaskForm from './NewTaskForm';
+import TaskList from './TaskList';
+import { v4 as uuidv4 } from 'uuid';
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import { CATEGORIES, TASKS } from '../data';
+// console.log("Here's the data you're working with");
+// console.log({ CATEGORIES, TASKS });
 
 function App() {
+  let taskArray = TASKS.map((task) => {
+    return { ...task, id: uuidv4() };
+  });
+
+  const [tasks, setTasks] = useState(taskArray);
+  console.log(tasks);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
       <CategoryFilter />
       <NewTaskForm />
-      <TaskList />
+      {/* <TaskList tasks={tasks} setTasks={setTasks} /> */}
     </div>
   );
 }
